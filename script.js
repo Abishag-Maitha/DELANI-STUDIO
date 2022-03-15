58d111fc912fbddd5ec553200ca55c17cb7ac9de
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-03-15 20:27:25 +0300

    Add changes to css, js and html files

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
         $(this).find("p").slideToggle(500).closest(".bt").find("img").slideToggle(700)
       
     });
-  });
+    // Hover effects on cards
+    let cards=$(".crd");
+    cards.hover(function(){
+        $(this).find("p").toggle()
+    })
+        
+    });
